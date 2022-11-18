7a6de77f9392e7fe6993edb00a848b948405a63a
33ZTH <elizabeth.casua@gmail.com>
2022-11-18 17:13:24 +0100

    Changes on: prefabs, common enemy, gameManager, Camera and Player
    - Prefab sphere has been removed definitively and keep the polyhedron as final bullet.
    - Cube has been deleted and replaced by the new common enemy, Slime. Now is the slime who change material/color when die.
    - I add two versions of gameManagerScript.AddCoin(); (PlayerMovement Script 53 line, but I couldn't get the most optimal version to run. So, I left it commented.
    - CameraController Script added. Now camera is following the player. Optimal version.
    - Changes on PlayerMovement Scrip:
      a) I tried differents ways of movements using transform. But finally I did the movement with MovePosition and accessing to rigidbody component.
    - Starting with animator to introduce animations through code/animator:
      a) Set IDLE
      b) Set walking

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,33 +7,68 @@ public class PlayerMovement : MonoBehaviour
     public int speed;
     public int turnSpeed;
 
+    float h, v;
+
+    //This variable stores the direction of movement
+    Vector3 movement;
+    Rigidbody rb;
+    Animator anim;
+
     public GameObject gameManager;
     //public GameObject gameManagerScript;
 
-    float h, v;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
- 
     void Update()
     {
         InputPlayer();
-        Movement();
-        Rotate();
+        //MovementTransform();
+        //Rotate();
+        Animating();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
     }
 
     void InputPlayer()
     {
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
+        movement = new Vector3(h, 0, v);
+        //To avoid the problem that it goes faster diagonally we have to normalize.
+        movement.Normalize();
     }
 
-    void Movement()
+    void Move()
     {
-        transform.Translate(Vector3.forward * speed * v * Time.deltaTime);
+        rb.MovePosition(transform.position + (movement * speed * Time.deltaTime));
+    }
+
+    void MovementTransform()
+    {
+        /*Don't forget this:
+        * 
+        * Vector3.forward = (0,0,1)
+        * Vector3.right = (1,0,0)
+        * 
+        * movement = (0,0,1) - Teclas WS
+        * movement = (1,0,0) - Teclas AD
+        * movement = (1,0,1) - Teclas WD
+        * 
+        * transform.Translate(Vector3.forward * speed * v * Time.deltaTime);
+        * transform.Translate(Vector3.right * speed * h * Time.deltaTime);
+        * 
+        * But we can do this in one line y we can eliminate the method Rotate too
+        */
+        transform.Translate(movement * speed * Time.deltaTime);
+
     }
 
     void Rotate()
@@ -41,13 +76,32 @@ public class PlayerMovement : MonoBehaviour
         transform.Rotate(Vector3.up * turnSpeed * h * Time.deltaTime);
     }
 
+    void Animating()
+    {
+        //If input horizontal or vertical are different to zero
+        if(h !=0 || v !=0)
+        {
+            //Then player is moving and this condition is true and the animation runs
+            anim.SetBool("IsMoving", true);
+        }
+        else
+        {
+            //Then player is not moving and this condition is false and the animation doesn't runs
+            anim.SetBool("IsMoving", false);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("ManaCoin"))
         {
-            //To access GameManager to get AddCoin method
+            /*To access GameManager to get AddCoin method
+             gameManager is a variable of Gameobject type*/
             gameManager.GetComponent<GameManager>().AddCoin();
-            //gameManagerScript.AddCoin();
+
+            /*gameManagerScript is a variable GameManager type
+            gameManagerScript.AddCoin();*/
+           
 
             Destroy(other.gameObject);
         }
